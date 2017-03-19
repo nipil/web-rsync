467d61332b478730561e14ed096b6ce8705f9c4a
nipil <nipil@users.noreply.github.com>
2017-03-19 22:39:56 +0100

    start of chacha20

--- src/index.php
+++ src/index.php
@@ -1,15 +1,45 @@
 <?php
 
-function do_client() {
-    echo "client\n";
+class ChaCha20 {
+
+    private $key;
+    private $nonce;
+
+    public function __construct($key, $nonce) {
+        if (strlen($key) !== 32) {
+            throw new Exception('Key must be a 256-bit string');
+        }
+        if (strlen($nonce) !== 12) {
+            throw new Exception('Nonce must be a 96-bit string');
+        }
+        $this->key = $key;
+        $this->nonce = $nonce;
+    }
 }
 
-function do_server() {
-    echo "server\n";
+class Main {
+
+    public function do_client() {
+        $options = getopt("k");
+        if (array_key_exists("k", $options)) {
+            $cipher = new ChaCha20(
+                "12345678901234567890123456789012",
+                "123456789012"
+            );
+        }
+    }
+
+    public function do_server() {
+    }
+
+    public function run() {
+        if (php_sapi_name() === 'cli' or defined('STDIN')) {
+            $this->do_client();
+        } else {
+            $this->do_server();
+        }
+    }
 }
 
-if (php_sapi_name() === 'cli' or defined('STDIN')) {
-    do_client();
-} else {
-    do_server();
-}
+$main = new Main();
+$main->run();

b6db76dfe696040609835d2b34834c56d6f0351a
nipil <nipil@users.noreply.github.com>
2017-03-24 05:03:21 +0100

    add first test

---
+++ test/ChaCha20BlockTest.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @covers ChaCha20BlockTest
+ */
+final class ChaCha20BlockTest extends PHPUnit\Framework\TestCase
+{
+    public function testTrue() /* add ': void' in php 7.1 */
+    {
+        $this->assertTrue(TRUE);
+    }
+}

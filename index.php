61022db4a9bd4f22fa257006e96f0294b33fa1a4
nipil <nipil@users.noreply.github.com>
2017-04-08 12:17:03 +0200

    Use external ChaCha20 component

--- index.php
+++ index.php
@@ -2,15 +2,17 @@
 
 require_once __DIR__.'/vendor/autoload.php';
 
-$cipher = new WRS\ChaCha20Block();
+use ChaCha20\ChaCha20Block;
+
+$cipher = new ChaCha20Block();
 $cipher->set_key(hex2bin("000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f"));
 $cipher->set_nonce(hex2bin("000000090000004a00000000"));
 $cipher->set_counter(1);
 
-echo "init\n";
-echo $cipher;
+printf("BEG: %s\n", bin2hex(
+    $cipher->serialize_state(ChaCha20Block::STATE_INITIAL)));
 
 $cipher->compute_block();
 
-echo "final\n";
-echo $cipher;
+printf("END: %s\n", bin2hex(
+    $cipher->serialize_state(ChaCha20Block::STATE_FINAL)));
--- src/ChaCha20Exception.php
+++
@@ -1,6 +0,0 @@
-<?php namespace WRS;
-
-/**
- * Exceptions specific to ChaCha20
- */
-class ChaCha20Exception extends \Exception {}

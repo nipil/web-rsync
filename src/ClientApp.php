29a60f2e514fef9bd4087fe0770fd4885fd495fe
nipil <nipil@users.noreply.github.com>
2017-05-01 12:01:12 +0200

    add client/server master classes

---
+++ src/ClientApp.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace WRS;
+
+class ClientApp {
+
+    public function __construct() {
+    }
+
+    public function run() {
+        echo "client\n";
+    }
+}
---
+++ src/ServerApp.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace WRS;
+
+class ServerApp {
+    public function __construct() {
+    }
+
+    public function run() {
+        echo "server\n";
+    }
+}

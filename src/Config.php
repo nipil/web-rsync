0ca6d24735eaef9b11a214b502758b8129076ec7
nipil <nipil@users.noreply.github.com>
2017-05-11 17:38:56 +0200

    rewrite Config to split access and storage

--- src/Config.php
+++ src/Config.php
@@ -4,100 +4,62 @@ declare(strict_types=1);
 
 namespace WRS;
 
-class Config {
-
-    const CONFIG_FILE = "wrs_config.php";
+class Config implements KeyValueInterface {
 
     private $logger;
-    private $base_path;
+    private $storage;
     private $data;
+    private $definitions;
 
-    public function __construct(string $base_path) {
+    public static function StringToInt(string $input) {
+        $valid = preg_match('/^-?[[:digit:]]+$/', $input);
+        if ($valid === 0) {
+            throw new \Exception(sprintf(
+                "Invalid integer %s",
+                $input));
+        }
+        $result = sscanf($input, "%d", $value);
+        if ($result === 0) {
+            throw new \Exception(sprintf(
+                "Cannot convert %s to an integer",
+                $name));
+        }
+        return $value;
+    }
+
+    public function __construct(StorageInterface $storage) {
         $this->logger = App::GetLogger();
         $this->logger->debug(__METHOD__, func_get_args());
-        $this->base_path = $base_path;
+        $this->storage = $storage;
         $this->data = array();
     }
 
-    public function get_data() {
-        return $this->data;
+    public function has_key(string $key) {
+        return array_key_exists($key, $this->data);
     }
 
-    public function get_default_config_file() {
-        return $this->base_path
-            . DIRECTORY_SEPARATOR
-            . self::CONFIG_FILE;
-    }
-
-    public function save() {
-        $this->logger->debug(__METHOD__);
-        // build config file path
-        $filepath = $this->get_default_config_file();
-        // generate text
-        $txt = '<?php'.PHP_EOL.'return '.var_export($this->data, true).';'.PHP_EOL;
-        // save key configuration
-        $res = file_put_contents($filepath, $txt);
-        if ($res === FALSE) {
-            throw new \Exception(sprintf(
-                "Cannot save config file %s",
-                $filepath));
+    public function get_string(string $key) {
+        if (!$this->has_key($key)) {
+            $this->data[$key] = $this->storage->load($key);
         }
+        return $this->data[$key];
     }
 
-    public function load_default_optional() {
-        $this->logger->debug(__METHOD__);
-        // build config file path
-        $filepath = $this->get_default_config_file();
-        if (!file_exists($filepath)) {
-            $this->logger->info("No configuration file found, using default values");
-            $this->data = array();
-            return;
-        }
-        // load data
-        $data = @include($filepath);
-        // which may fail because
-        if (gettype($data) !== "array") {
-            throw new \Exception(sprintf(
-                "Invalid configuration file %s",
-                $filepath));
-        }
-        $this->data = $data;
-        $this->logger->debug("Default config loaded", $this->data);
+    public function set_string(string $key, string $value) {
+        $this->storage->save($key, $value);
+        $this->data[$key] = $value;
     }
 
-    public function load_custom_required(string $filepath) {
-        $this->logger->debug(__METHOD__, func_get_args());
-        if (!file_exists($filepath)) {
-            throw new \Exception(sprintf(
-                "Configuration file %s not found",
-                $filepath));
+    public function get_integer(string $key) {
+        if (!$this->has_key($key)) {
+            $input = $this->storage->load($key);
+            $this->data[$key] = self::StringToInt($input);
         }
-        $data = @include($filepath);
-        if (gettype($data) !== "array") {
-            throw new \Exception(sprintf(
-                "Invalid configuration file %s",
-                $filepath));
-        }
-        // store loaded data
-        $this->data = $data;
-        $this->logger->debug("Custom config loaded", $this->data);
+        return $this->data[$key];
     }
 
-    public function get(string $name) {
-        $this->logger->debug(__METHOD__, func_get_args());
-        if (!isset($this->data[$name])) {
-            return NULL;
-        }
-        return $this->data[$name];
-    }
-
-    public function set_integer(string $name, int $value) {
-        $this->logger->debug(__METHOD__, func_get_args());
-        $this->data[$name] = $value;
-    }
-
-    public function set_string(string $name, string $text) {
-        $this->logger->debug(__METHOD__, func_get_args());
-        $this->data[$name] = $text;
+    public function set_integer(string $key, int $value) {
+        $this->storage->save($key, sprintf("%d", $value));
+        $this->data[$key] = $value;
     }
 }
--- test/ConfigTest.php
+++ test/ConfigTest.php
@@ -6,169 +6,95 @@ namespace WRS;
 
 use PHPUnit\Framework\TestCase;
 
-use org\bovigo\vfs\vfsStream, org\bovigo\vfs\vfsStreamWrapper, org\bovigo\vfs\vfsStreamDirectory;
-
 class ConfigTest extends TestCase
 {
-    const SAMPLE_INPUT_VALID = "<?php" . PHP_EOL
-        . "return array (" . PHP_EOL
-        . "  'integer' => 42," . PHP_EOL
-        . "  'string' => 'text'," . PHP_EOL
-        . ");" . PHP_EOL;
+    const KEY = "test";
+    const VALUE_STRING = "value";
+    const VALUE_INT = 42;
 
-    const SAMPLE_INPUT_NOT_AN_ARRAY = "toto";
-
-    public function setUp()
-    {
-        vfsStreamWrapper::register();
-        vfsStreamWrapper::setRoot(new vfsStreamDirectory('baseDirectory'));
-    }
-
-    public function setup_config_content(string $content) {
-        $this->assertFalse(
-            vfsStreamWrapper::getRoot()->hasChild(Config::CONFIG_FILE),
-            "Config file already present");
-        file_put_contents(
-            vfsStream::url(
-                'baseDirectory'
-                . DIRECTORY_SEPARATOR
-                . Config::CONFIG_FILE),
-            $content);
-        $this->assertTrue(
-            vfsStreamWrapper::getRoot()->hasChild(Config::CONFIG_FILE),
-            "Config file is absent");
-        $this->assertEquals(file_get_contents(
-            vfsStream::url(
-                'baseDirectory'
-                . DIRECTORY_SEPARATOR
-                . Config::CONFIG_FILE)),
-            $content);
-    }
-
-    public function testConfigLoadDefaultOptionalWithoutFile() {
-        $config = new Config(vfsStream::url('baseDirectory'));
-        $this->assertFalse(
-            vfsStreamWrapper::getRoot()->hasChild(Config::CONFIG_FILE),
-            "config file already present");
-        $config->load_default_optional();
-        $this->assertEquals($config->get_data(), array(), "Default configuration is incorrect");
-    }
-
-    public function testConfigLoadDefaultOptionalWithFile() {
-        $config = new Config(vfsStream::url('baseDirectory'));
-        $this->setup_config_content(self::SAMPLE_INPUT_VALID);
-        $config->load_default_optional();
-        $this->assertEquals(
-            $config->get_data(),
-            array (
-                'integer' => 42,
-                'string' => 'text'
-                ),
-            "Loaded configuration is incorrect");
+    public function providerStringToIntValid() {
+        $data = array(
+            "zero" => ["0", 0],
+            "zero minus" => ["-0", 0],
+            "five minus" => ["-5", -5],
+            "five" => ["5", 5],
+            "int max 32 bits" => [ "2147483647",  2147483647],
+            "int min 32 bits" => ["-2147483648", -2147483648],
+        );
+        if (PHP_INT_SIZE === 8) {
+            $data = array_merge($data, array(
+                "int max 64 bits" => [ "9223372036854775807",  9223372036854775807],
+                "int min 64 bits" => ["-9223372036854775808", -9223372036854775808],
+            ));
+        }
+        return $data;
     }
 
     /**
-     * @expectedException        Exception
-     * @expectedExceptionMessage Invalid configuration file vfs://baseDirectory/wrs_config.php
+     * @dataProvider providerStringToIntValid
      */
-    public function testConfigLoadDefaultOptionalWithFileInvalidContent() {
-        $config = new Config(vfsStream::url('baseDirectory'));
-        $this->setup_config_content(self::SAMPLE_INPUT_NOT_AN_ARRAY);
-        $config->load_default_optional();
+    public function testStringToIntSuccess(string $input, int $expected) {
+        $value = Config::StringToInt($input);
+        $this->assertSame($expected, $value);
+    }
+
+    public function providerStringToIntInvalid() {
+        $data = array(
+            "zero" => ["+0", 1],
+            "text" => ["text", 1],
+            "mixed" => ["36mix", 1],
+            "non-trimmed" => [" \t 6    \t", 1],
+        );
+        return $data;
     }
 
     /**
-     * @expectedException        Exception
-     * @expectedExceptionMessage Configuration file vfs://baseDirectory/non_existing_file.php not found
+     * @dataProvider      providerStringToIntInvalid
+     * @expectedException Exception
      */
-    public function testConfigLoadCustomRequiredWithoutFile() {
-        $config = new Config(vfsStream::url('baseDirectory'));
-        $config->load_custom_required(
-            vfsStream::url(
-                'baseDirectory'
-                . DIRECTORY_SEPARATOR
-                . "non_existing_file.php"));
+    public function testStringToIntFail(string $input, int $expected) {
+        $value = Config::StringToInt($input);
+        $this->assertSame($expected, $value);
     }
 
-    public function testConfigLoadCustomRequiredWithFile() {
-        $config = new Config(vfsStream::url('baseDirectory'));
-        $this->setup_config_content(self::SAMPLE_INPUT_VALID);
-        $config->load_custom_required(
-            vfsStream::url(
-                'baseDirectory'
-                . DIRECTORY_SEPARATOR
-                . Config::CONFIG_FILE));
-        $this->assertEquals(
-            $config->get_data(),
-            array (
-                'integer' => 42,
-                'string' => 'text'
-                ),
-            "Loaded configuration is incorrect");
+    public function testHasKey() {
+        $config = new Config(new NullStorage());
+        $this->assertFalse($config->has_key(self::KEY), "key should not exist");
+        $config->set_integer(self::KEY, self::VALUE_INT);
+        $this->assertTrue($config->has_key(self::KEY), "key should exist");
+    }
+
+    public function testSetString() {
+        $config = new Config(new NullStorage());
+        $config->set_string(self::KEY, self::VALUE_STRING);
+        $value = $config->get_string(self::KEY);
+        $this->assertSame(self::VALUE_STRING, $value);
     }
 
     /**
+     * @dataProvider             providerStringToIntInvalid
      * @expectedException        Exception
-     * @expectedExceptionMessage Invalid configuration file vfs://baseDirectory/wrs_config.php
+     * @expectedExceptionMessage Cannot load key test
      */
-    public function testConfigLoadCustomRequiredWithFileInvalidContent() {
-        $config = new Config(vfsStream::url('baseDirectory'));
-        $this->setup_config_content(self::SAMPLE_INPUT_NOT_AN_ARRAY);
-        $config->load_custom_required(
-            vfsStream::url(
-                'baseDirectory'
-                . DIRECTORY_SEPARATOR
-                . Config::CONFIG_FILE));
+    public function testGetStringFail() {
+        $config = new Config(new NullStorage());
+        $config->get_string(self::KEY);
     }
 
-    public function testConfigSetInt() {
-        $key = "integer";
-        $value = 42;
-        $config = new Config(vfsStream::url('baseDirectory'));
-        $config->set_integer($key, $value);
-        $this->assertEquals(
-            $config->get($key),
-            $value,
-            "invalid config value");
+    public function testSetInteger() {
+        $config = new Config(new NullStorage());
+        $config->set_integer(self::KEY, self::VALUE_INT);
+        $value = $config->get_integer(self::KEY);
+        $this->assertSame(self::VALUE_INT, $value);
     }
 
-    public function testConfigSetText() {
-        $key = "string";
-        $value = "text";
-        $config = new Config(vfsStream::url('baseDirectory'));
-        $config->set_string($key, $value);
-        $this->assertEquals(
-            $config->get($key),
-            $value,
-            "invalid config value");
-    }
-
-    public function testConfigGetInexistant() {
-        $config = new Config(vfsStream::url('baseDirectory'));
-        $value = $config->get("inexistant");
-        $this->assertNull($value,
-            "Non-existing configuration should be NULL");
-    }
-
-    public function testConfigSave() {
-        $config = new Config(vfsStream::url('baseDirectory'));
-        $config->set_integer("integer", 42);
-        $config->set_string("string", "text");
-        $this->assertFalse(
-            vfsStreamWrapper::getRoot()->hasChild(Config::CONFIG_FILE),
-            "config file already present");
-
-        $config->save();
-        $this->assertTrue(
-            vfsStreamWrapper::getRoot()->hasChild(Config::CONFIG_FILE),
-            "config file is absent");
-
-        $content = file_get_contents(
-            vfsStream::url(
-                'baseDirectory'
-                . DIRECTORY_SEPARATOR
-                . Config::CONFIG_FILE));
-        $this->assertNotSame(FALSE, $content, "content is FALSE");
-        $this->assertEquals(self::SAMPLE_INPUT_VALID, $content, "output differ");
+    /**
+     * @dataProvider             providerStringToIntInvalid
+     * @expectedException        Exception
+     * @expectedExceptionMessage Cannot load key test
+     */
+    public function testGetIntegerFail() {
+        $config = new Config(new NullStorage());
+        $config->get_integer(self::KEY);
     }
 }

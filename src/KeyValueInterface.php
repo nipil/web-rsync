108e341d9483a75f3467b93a742eb110c8a670d0
nipil <nipil@users.noreply.github.com>
2017-05-11 15:40:46 +0200

    add KeyValueInterface for everything related to data access

---
+++ src/KeyValueInterface.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace WRS;
+
+interface KeyValueInterface {
+    public function has_key(string $key);
+    public function get_integer(string $key);
+    public function set_integer(string $key, int $value);
+    public function get_string(string $key);
+    public function set_string(string $key, string $value);
+}

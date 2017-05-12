70ee8f5ed5ade2e6610428c9e27f56329e179e92
nipil <nipil@users.noreply.github.com>
2017-05-13 01:27:14 +0200

    code presentation: do not split single lines too much

--- src/Crypto/KeyManager.php
+++ src/Crypto/KeyManager.php
@@ -31,11 +31,7 @@ class KeyManager {
         }
 
         // extract phase (with 2.1 note : 'IKM' is used as the HMAC input, not as the HMAC key)
-        $prk = hash_hmac(
-            self::HASH_FUNCTION,
-            $this->get_master_key(),
-            $this->get_master_salt(),
-            TRUE);
+        $prk = hash_hmac(self::HASH_FUNCTION, $this->get_master_key(), $this->get_master_salt(), TRUE);
 
         // handles different hashing functions
         $len = strlen($prk);
@@ -58,57 +54,38 @@ class KeyManager {
         $hex = bin2hex($bin);
         $len = strlen($bin);
         if ($len != $req_len) {
-            throw new \Exception(sprintf(
-                "Invalid length for %s : %s",
-                $name,
-                $len));
+            throw new \Exception(sprintf("Invalid length for %s : %s", $name, $len));
         }
         $this->config->set_string($name, $hex);
     }
 
     public function set_master_key(string $key) {
-        return $this->bin_to_hex(
-            self::CONFIG_NAME_KEY,
-            self::MASTER_KEY_LENGTH_BYTES,
-            $key);
+        return $this->bin_to_hex(self::CONFIG_NAME_KEY, self::MASTER_KEY_LENGTH_BYTES, $key);
     }
 
     public function set_master_salt(string $salt) {
-        return $this->bin_to_hex(
-            self::CONFIG_NAME_SALT,
-            self::MASTER_SALT_LENGTH_BYTES,
-            $salt);
+        return $this->bin_to_hex(self::CONFIG_NAME_SALT, self::MASTER_SALT_LENGTH_BYTES, $salt);
     }
 
     protected function hex_to_bin(string $name, int $req_len) {
         $hex = $this->config->get_string($name);
         $bin = @hex2bin($hex);
         if ($bin === FALSE) {
-            throw new \Exception(sprintf(
-                "Invalid hex string %s : %s",
-                $name,
-                $hex));
+            throw new \Exception(sprintf("Invalid hex string %s : %s", $name, $hex));
         }
         $len = strlen($bin);
         if ($len != $req_len) {
-            throw new \Exception(sprintf(
-                "Invalid length for %s : %s",
-                $name,
-                $len));
+            throw new \Exception(sprintf("Invalid length for %s : %s", $name, $len));
         }
         return $bin;
     }
 
     public function get_master_key() {
-        return $this->hex_to_bin(
-            self::CONFIG_NAME_KEY,
-            self::MASTER_KEY_LENGTH_BYTES);
+        return $this->hex_to_bin(self::CONFIG_NAME_KEY, self::MASTER_KEY_LENGTH_BYTES);
     }
 
     public function get_master_salt() {
-        return $this->hex_to_bin(
-            self::CONFIG_NAME_SALT,
-            self::MASTER_SALT_LENGTH_BYTES);
+        return $this->hex_to_bin(self::CONFIG_NAME_SALT, self::MASTER_SALT_LENGTH_BYTES);
     }
 
     public function __construct(KeyValueInterface $config) {

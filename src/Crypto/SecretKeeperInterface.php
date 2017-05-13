e0576f0bfaa49960ea808101a3019910c0322927
nipil <nipil@users.noreply.github.com>
2017-05-13 13:23:44 +0200

    create SecretKeeperInterface interface

---
+++ src/Crypto/SecretKeeperInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace WRS\Crypto;
+
+interface SecretKeeperInterface {
+    public function set_key(string $key);
+    public function set_salt(string $salt);
+    public function get_key();
+    public function get_salt();
+}

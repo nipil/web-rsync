6551d6a5eeddde1d12a06c28ce7fd87cd3a7bca5
nipil <nipil@users.noreply.github.com>
2017-05-13 15:29:07 +0200

    create NativeHasher+Test implementing hashing interface

---
+++ src/Crypto/NativeHasher.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace WRS\Crypto;
+
+use WRS\Crypto\HashInterface;
+
+class NativeHasher implements HashInterface {
+
+    private $hash_function;
+
+    public function __construct(string $hash_function) {
+        if (!in_array($hash_function, hash_algos(), TRUE)) {
+            throw new \Exception(sprintf("Invalid hash function : %s", $hash_function));
+        }
+        $this->hash_function = $hash_function;
+    }
+
+    public function get_hash_function() {
+        return $this->hash_function;
+    }
+
+    public function hash(string $message) {
+        return hash($this->hash_function, $message, TRUE);
+    }
+
+    public function hmac(string $message, string $key) {
+        return hash_hmac($this->hash_function, $message, $key, TRUE);
+    }
+}
---
+++ test/NativeHasherTest.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace WRS\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+use WRS\Crypto\NativeHasher;
+
+class NativeHasherTest extends TestCase
+{
+    const FUNC = "md5";
+    const MSG = "The quick brown fox jumps over the lazy dog";
+    const HASH = "9e107d9d372bb6826bd81d3542a419d6";
+    const KEY = "key";
+    const HMAC = "80070713463e7749b90c2dc24911e275";
+
+    public function providerAlgo() {
+        return array_map(function($algo) { return [$algo, NULL]; }, hash_algos());
+    }
+
+    /**
+     * @dataProvider providerAlgo
+     */
+    public function testConstructorValid(string $algo, $null) {
+        $nh = new NativeHasher($algo);
+        $this->assertSame($algo, $nh->get_hash_function());
+    }
+
+    /**
+     * @expectedException Exception
+     * @expectedExceptionMessageRegExp #^Invalid hash function : .*$#
+     */
+    public function testConstructorInvalid() {
+        $nh = new NativeHasher("this_is_an_invalid_hash_function");
+    }
+
+    public function testHash() {
+        $nh = new NativeHasher(self::FUNC);
+        $result = $nh->hash(self::MSG);
+        $this->assertSame(hex2bin(self::HASH), $result);
+    }
+
+    public function testHmac() {
+        $nh = new NativeHasher(self::FUNC);
+        $result = $nh->hmac(self::MSG, self::KEY);
+        $this->assertSame(hex2bin(self::HMAC), $result);
+    }
+}

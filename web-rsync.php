6805c66a3490b8ce7b543464838c4aedf8f0de72
nipil <nipil@users.noreply.github.com>
2017-05-15 20:40:38 +0200

    update master script

--- web-rsync.php
+++ web-rsync.php
@@ -2,23 +2,75 @@
 
 require_once __DIR__.'/vendor/autoload.php';
 
-use WRS\Apps\App,
-    WRS\Apps\ClientApp,
-    WRS\Apps\ServerApp;
+use Monolog\Logger;
+
+use WRS\Apps\ClientApp;
+use WRS\Apps\ServerApp;
+
+use WRS\Crypto\NativeHasher;
+use WRS\Crypto\NativeRandomizer;
+use WRS\Crypto\HmacKeyDerivator;
+use WRS\Crypto\MasterSecret;
+
+use WRS\KeyValue\CachedKeyValue;
+use WRS\KeyValue\StoredKeyValue;
+
+use WRS\Storage\FileStorage;
+
+use WRS\Arguments;
 
 try {
-    // configure logger
-    App::SetLogger(new \Monolog\Logger('web-rsync'));
-    // same script handles client side and server side
-    if (php_sapi_name() === 'cli' OR defined('STDIN')) {
-        $app = new ClientApp(__DIR__);
+
+    // Common setup for client and server
+
+    $hasher = new NativeHasher("sha512");
+
+    $randomizer = new NativeRandomizer();
+
+    $config = new CachedKeyValue(
+        new StoredKeyValue(
+            new FileStorage(
+                __DIR__ . DIRECTORY_SEPARATOR . "conf"
+            )
+        )
+    );
+
+    $master_secret = new MasterSecret(
+        "master",
+        1024, // key length in bytes
+        16,   // salt length in bytes
+        $config,
+        $randomizer
+    );
+
+    if (php_sapi_name() === 'cli' or defined('STDIN')) {
+
+        // Client specific setup
+        $arguments = new Arguments();
+        $logger = new Logger('client');
+
+        // Run client
+        $app = new ClientApp(
+            $arguments,
+            $master_secret,
+            $logger
+        );
         $result = $app->run();
+
+        // End processing after client has run
         exit($result);
+
     } else {
-        $app = new ServerApp(__DIR__);
+
+        // Server specific setup
+        $logger = new Logger('server');
+
+        // Run server-side
+        $app = new ServerApp($logger);
         $app->run();
+
     }
+
 } catch (\Exception $e) {
-    $main_logger = App::GetLogger("main");
-    $main_logger->error($e);
+    trigger_error($e);
 }
